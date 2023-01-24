45abee93f93694190bbbec903a1628b05d0a5af3
Jayanthi C <sivaranjanijayanthi@gmail.com>
2023-01-24 22:35:10 +0530

    homescreen

--- OnboardApp/onboard_app/lib/main.dart
+++ OnboardApp/onboard_app/lib/main.dart
@@ -4,3 +4,31 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'DemoApp',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    );
+  }
+}
