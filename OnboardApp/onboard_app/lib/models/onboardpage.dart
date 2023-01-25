dbb69502b581e1bc27396a530892bf69e72ddab1
Jayanthi C <sivaranjanijayanthi@gmail.com>
2023-01-25 09:55:12 +0530

    getters and setters in onboardpage model

--- OnboardApp/onboard_app/lib/models/onboardpage.dart
+++ OnboardApp/onboard_app/lib/models/onboardpage.dart
@@ -1,9 +1,33 @@
 class OnboardPageModel {
-  String? assestsPath;
+  String? assetsPath;
   String? title;
   String? description;
 
-  OnboardPageModel({this.assestsPath, this.title, this.description});
+  OnboardPageModel({this.assetsPath, this.title, this.description});
+
+  void setassetsPath(String getassetsPath) {
+    assetsPath = getassetsPath;
+  }
+
+  void settitle(String gettitle) {
+    title = gettitle;
+  }
+
+  void setdescription(String getdescription) {
+    description = getdescription;
+  }
+
+  String? getassetsPath() {
+    return assetsPath;
+  }
+
+  String? gettitel() {
+    return title;
+  }
+
+  String? getdescription() {
+    return description;
+  }
 }
 
 List<OnboardPageModel> getPages() {}
