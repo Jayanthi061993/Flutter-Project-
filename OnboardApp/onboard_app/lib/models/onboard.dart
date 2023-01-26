f434c4677fd3dfe7e821af915302f584f1bb70cb
Jayanthi C <sivaranjanijayanthi@gmail.com>
2023-01-26 22:46:08 +0530

    page index dots

--- OnboardApp/onboard_app/lib/models/onboard.dart
+++ OnboardApp/onboard_app/lib/models/onboard.dart
@@ -36,7 +36,7 @@ List<OnboardModel> getPages() {
   //Page1
   OnboardModel onboardpage = OnboardModel();
 
-  onboardpage.setAssetsPath('assets/bank.png');
+  onboardpage.setAssetsPath('assets/Free illustration 1.png');
   onboardpage.setTitle('Save Your Balance');
   onboardpage.setDescription(
       'Providing with Best Solution to save your balance and transactions');
@@ -46,7 +46,7 @@ List<OnboardModel> getPages() {
   //Page2
   onboardpage = OnboardModel();
 
-  onboardpage.setAssetsPath('assets/mobile-banking.png');
+  onboardpage.setAssetsPath('assets/Free illustration 2.png');
   onboardpage.setTitle('Make it Simple');
   onboardpage.setDescription(
       'We pay attention to all your payments and find way to save your money');
@@ -56,7 +56,7 @@ List<OnboardModel> getPages() {
   //Page3
   onboardpage = OnboardModel();
 
-  onboardpage.setAssetsPath('assets/credit-card.png');
+  onboardpage.setAssetsPath('assets/Free illustration 3.png');
   onboardpage.setTitle('New Banking');
   onboardpage.setDescription(
       'Providing with Free advisory service, mobile banking application, Visa debit Cards');
